1b7d3e3fd6e2fd6666548dadaa9a845f1c25c460
Tom Murray <tomkeohanemurray@gmail.com>
2024-05-16 12:41:23 -0500

    connect db

--- worker.py
+++ worker.py
@@ -1,6 +1,15 @@
 import asyncio
+from database.mongo import DatabaseClient
 from metrics.collect import collect_metrics_with_retries
 
 
+async def main():
+    await DatabaseClient.connect()
+    await DatabaseClient.create_indexes()
+
+    await collect_metrics_with_retries()
+
+    await DatabaseClient.disconnect()
+
 if __name__ == "__main__":
-    asyncio.run(collect_metrics_with_retries())
+    asyncio.run(main())
