b414d0aeb365e2b61d0a9b14563d777e9d36a94e
Amy Deng <kexindeng2000@gmail.com>
2024-07-09 15:57:19 -0700

    remove unsupported model by perplexity

--- providers/concrete_providers/perplexity.py
+++ providers/concrete_providers/perplexity.py
@@ -6,6 +6,5 @@ class Perplexity(BaseProvider):
         "llama-3-8b-chat": "perplexity/llama-3-8b-instruct",
         "llama-3-70b-chat": "perplexity/llama-3-70b-instruct",
         "mixtral-8x7b": "perplexity/mixtral-8x7b-instruct",
-        "mistral-7b-chat": "perplexity/mistral-7b-instruct",
     }
     PROVIDER_NAME = "Perplexity"
