410e56bca2c6c57bc6a6697e5e39f9ef5b47e992
Amy Deng <kexindeng2000@gmail.com>
2024-07-09 15:57:02 -0700

    correct model paths

--- providers/concrete_providers/anthropic.py
+++ providers/concrete_providers/anthropic.py
@@ -3,8 +3,8 @@ from providers.abstract_providers.base_provider import BaseProvider
 
 class Anthropic(BaseProvider):
     ADAPTER_MODEL_STR_MAP = {
-        "claude-3-sonnet": "claude-3-sonnet-20240229",
-        "claude-3-opus": "claude-3-opus-20240229",
-        "claude-3-haiku": "claude-3-haiku-20240307",
+        "claude-3-sonnet": "anthropic/claude-3-sonnet-20240229",
+        "claude-3-opus": "anthropic/claude-3-opus-20240229",
+        "claude-3-haiku": "anthropic/claude-3-haiku-20240307",
     }
     PROVIDER_NAME = "Anthropic"
--- providers/concrete_providers/openai.py
+++ providers/concrete_providers/openai.py
@@ -3,8 +3,9 @@ from providers.abstract_providers.base_provider import BaseProvider
 
 class Openai(BaseProvider):
     ADAPTER_MODEL_STR_MAP = {
-        "gpt-4-turbo": "gpt-4-turbo",
-        "gpt-3.5-turbo": "gpt-3.5-turbo",
-        "gpt-4": "gpt-4",
+        "gpt-4-turbo": "openai/gpt-4-turbo",
+        "gpt-3.5-turbo": "openai/gpt-3.5-turbo",
+        "gpt-4": "openai/gpt-4",
+        "gpt-4o": "openai/gpt-4o"
     }
     PROVIDER_NAME = "OpenAI"
